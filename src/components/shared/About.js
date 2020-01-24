f2c534c7134ad407b480b428f7e754dce62f8d5f
Mason Eyre <mason.deyre@gmail.com>
2020-01-23 18:21:28 -0700

    Update About.js

--- src/components/shared/About.js
+++ src/components/shared/About.js
@@ -9,9 +9,34 @@ const About = () => (
         It's a String Thing
       </p>
     </div>
-    <h3 style={{color: 'white', padding: 100}}>
-      This is a page that will talk about the school, it's mission/vision, and its owner, Rachel Harris.
-    </h3>
+    <p align='justify' style={{color: 'white', marginLeft: 50, marginRight: 60}}>
+      Hello,
+      <br/>
+      My name is Rachel, and I've been teaching for over 18 years.
+      Most likely you found my website either by pure luck or a link from Thumbtack.
+      Please feel free to take a look at my studio webpage, and contact me with any questions.
+    </p>
+    <ol align='justify' style={{color: 'white', marginLeft: 50, marginRight: 60}}>
+      Here are some of the things that I do for my students:
+      <li>
+        Finding a fully functioning instrument, that stays in tune and keeps
+        my students happy enough with the sound, that they have  chance at success.
+        This means I will go shopping with you to find the best instrument possible
+        for your individual price range. Because after all, it does have to sound decent
+        to begin with, but also has to be affordable!
+      </li>
+      <li>
+        Find a time for your weekly lesson with either me, or my associate teachers.
+      </li>
+      <li>
+        Get you ready to play in group lessons as soon as possible.
+        Anyone can say all they wat is to be a soloist, but in all honesty,
+        students must also be able to play with others to become a decent soloist.
+        This is why <b>group</b> lessons are such a priority for my students and me.
+        Group lessons also become everyone's favorite part
+        of taking lessons from my associates and me!
+      </li>
+    </ol>
   </div>
 )
 
